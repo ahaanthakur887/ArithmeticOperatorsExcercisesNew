46c62b38ea862a2a4c961d13cb95e3af1d98c817
ahaanthakur887 <thakura@stpaulsschool.org.uk>
2023-09-24 16:12:08 +0100

    All Questions Completed

--- IntroMac/Program.cs
+++ IntroMac/Program.cs
@@ -8,6 +8,9 @@
             Console.WriteLine(Area(10));
             Console.WriteLine(ExactlyDivisible(8, 4));
             Console.WriteLine($"The hypotenuse is {Pythagoras(3, 4)}");
+            Console.WriteLine(BooksPerStudent());
+            Console.WriteLine(BlocksToStore(1.1));
+            Console.WriteLine(PocketMoneySavings());
             Console.ReadLine();
         }
 
@@ -39,6 +42,35 @@
         {
             double output = Math.Pow(side1, 2) + Math.Pow(side2, 2);
             return Math.Sqrt(output);
+        }
+
+        static string BooksPerStudent()
+        {
+            Console.WriteLine("How many students are there?");
+            int students = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many books are there?");
+            int books = Convert.ToInt32(Console.ReadLine());
+            int DividedEqually = books / students;
+            int LeftOver = books - (DividedEqually * students);
+            return $"The number of books per student is : {DividedEqually}. The number of books left over is : {LeftOver}";
+        }
+
+        static int BlocksToStore(double FileSize)
+        {
+            double BlockSize = 512;
+            int output = Convert.ToInt32(Math.Ceiling((FileSize * 1024) / BlockSize));
+            return output;
+        }
+
+        static string PocketMoneySavings()
+        {
+            Console.WriteLine("How much pocket money do you get a week?");
+            double Money = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What percentage of this do you want to save?");
+            double Percentage = Convert.ToDouble(Console.ReadLine());
+            double SavingsPerWeek = Math.Round(Money * (Percentage / 100), 2);
+            double SavingsPerYear = SavingsPerWeek * 52;
+            return $"The amount of money saved per week is : {SavingsPerWeek}. The amount of money saved per year is : {SavingsPerYear}.";
         }
     }
 }
